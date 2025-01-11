4585c9ea6d2b65f99a0f8fd10cf2e5d9dc18c785
Mdanniyel <41233756+Mdanniyel@users.noreply.github.com>
2025-01-11 10:48:57 +0000

    init

--- svelte.config.js
+++ svelte.config.js
@@ -8,12 +8,10 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		 adapter: adapter({
-            // default options are shown
-            pages: 'build',
-            assets: 'build',
-            fallback: null
-          })
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter()
 	}
 };
 
