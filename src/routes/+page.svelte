507fb97c0e97fcf3ab3582877f8fd15036aedbc4
Mdanniyel <41233756+Mdanniyel@users.noreply.github.com>
2025-02-12 17:31:47 +0000

    add favicon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,21 @@
 <script>
-    import Hero from "$lib/sections/Hero.svelte";
-    import About from "$lib/sections/About.svelte";
+	import Hero from '$lib/sections/Hero.svelte';
+	import About from '$lib/sections/About.svelte';
 </script>
 
 <div class="h-20 bg-black"></div>
 
-<Hero/>
+<Hero />
 
-<About/>
+<About />
+
+<section class="container mx-auto max-w-lg py-20">
+	<h2 class="mb-8 text-center text-4xl font-extrabold tracking-tighter">איפה אני מלמד?</h2>
+	<p class="text-justify tracking-wide">
+		אני מלמד פרונטלית באזורים רמת גן, גבעתיים, פתח תקווה וקריית אונו, מתוך אמונה שהשיעור הפרונטלי
+		הוא הדרך האפקטיבית ביותר ללמידה. בנוסף, אני מציע גם שיעורים מרוחקים דרך המחשב, כך שאני יכול
+		לעזור לכם מכל מקום בארץ. עם זאת, אם באזור שלכם יש מורה פרונטלי טוב, אני ממליץ בחום לבחור באפשרות
+		הזו – איכות השיעור במפגש פנים אל פנים תמיד תהיה גבוהה יותר. בין אם פרונטלית ובין אם מרחוק, אני
+		כאן כדי לעזור לכם להצליח!
+	</p>
+</section>
